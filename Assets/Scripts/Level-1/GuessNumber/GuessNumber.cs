8c6c8ed5211a9f5d0725e7b8c2429a0d316b4144
CaptainBulba <varavaivan1@gmail.com>
2022-04-30 12:01:09 +0300

    unnecessary debug.log

--- Assets/Scripts/Level-1/GuessNumber/GuessNumber.cs
+++ Assets/Scripts/Level-1/GuessNumber/GuessNumber.cs
@@ -36,7 +36,6 @@ public class GuessNumber : MonoBehaviour
         {
             for (int i = 0; i < numbersRows[j].values.Length; i++) // 4
             {
-                Debug.Log(tableRowsAmount * j + i);
                 numberText[tableRowsAmount * j + i].GetComponentInChildren<Text>().text = numbersRows[j].values[i].ToString();
             }
         }
