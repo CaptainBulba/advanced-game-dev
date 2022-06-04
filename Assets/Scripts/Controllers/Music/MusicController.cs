c1bdd6b8ade8878070a4c19e47b247bc9924cab2
CaptainBulba <varavaivan1@gmail.com>
2022-06-04 13:39:32 +0300

    changed controller folder structure

---
+++ Assets/Scripts/Controllers/Music/MusicController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    public float maxVolume;
+    private float soundVolume = 1f;
+
+    private bool soundToggle = true;
+
+    public static MusicController Instance { get; private set; }
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (Instance != this)
+            Destroy(gameObject);
+    }
+
+    void Start()
+    {
+        audioSource.volume = maxVolume;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+            ChangeVolume();
+    }
+
+    private void ChangeVolume()
+    {
+        if (soundToggle) soundVolume = 0f;
+        else soundVolume = maxVolume;
+
+        soundToggle = !soundToggle;
+
+        audioSource.volume = soundVolume;
+    }
+
+    public float GetSoundVolume()
+    {
+        return soundVolume;
+    }
+
+    public void ChangeMusic(AudioClip clip)
+    {
+        audioSource.clip = clip;
+        audioSource.Play();
+    }
+}
