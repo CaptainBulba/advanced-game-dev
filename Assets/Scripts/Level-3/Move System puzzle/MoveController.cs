c6f95f86d0d7c78f2c939e54f9706f98a6b3f183
CaptainBulba <varavaivan1@gmail.com>
2022-06-06 18:50:55 +0300

    fixed MoveSystemPuzzle do not close bug

---
+++ Assets/Scripts/Level-3/Move System puzzle/MoveController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    public int counter;
+
+    private LevelController levelController;
+    private GameObject puzzleButton;
+    public PrefabSettings prefabSettings;
+
+    public void Start()
+    {
+
+        counter = MoveSystem.count;
+
+
+        puzzleButton = prefabSettings.GetComponent<PrefabSettings>().GetButton();
+        levelController = prefabSettings.GetComponent<PrefabSettings>().GetLevelController();
+
+    }
+    public void Update()
+    {
+        counter = MoveSystem.count;
+        if (counter == 3)
+        {
+            levelController.LaunchMainScreen(puzzleButton);
+        }
+    }
+}
